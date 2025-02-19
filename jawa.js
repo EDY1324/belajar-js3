5970a8bdeff0daf85fa212b64806607a54975b2e
EDY1324 <prabowoedy80@gmail.com>
2025-02-19 16:28:07 +0700

    API! API!

--- jawa.js
+++ jawa.js
@@ -1,12 +1,11 @@
-var bunga = ["Mawar", "Melatih", "Anggrek", "Sakura"];
+const apiKey = "zInF0zt7xgMzMiTrRPWdbhsZRxdahoLTRgTPKBwE";
+        const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
-bunga.push("Tulip", "Terompet", "Bangkai");
-bunga.splice()
-
-document.write("<h3>Daftar Nama-Nama Bunga:</h3>")
-document.write("<ol>");
-bunga.forEach((data) => {
-    document.write(`<li>${data}</li>`);
-    console.log(data)
-});
-document.write("</ol>");
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById("title").innerText = data.title;
+                document.getElementById("image").src = data.url;
+                document.getElementById("description").innerText = data.explanation;
+            })
+            .catch(error => console.error("Terjadi kesalahan:", error));
