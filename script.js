a991f03c2f7dc71796c64a0b4b54305d52724e5e
EDY1324 <prabowoedy80@gmail.com>
2025-02-27 09:21:51 +0700

    Tugas membuat Array yang berisi Object

--- script.js
+++ script.js
@@ -1,56 +1,24 @@
-//semua ini adalah pembelajan saya(Edy Prabowo) tentang JavaScript
-//ini adalah object dan properti pertama
-const person = {
-    name: "Aoi Kazuma",
-    age: 17,
-    tall: 170,
-    eyeColor: "Blue"
-};
-document.getElementById("p0").innerHTML = person.name + "," + person.tall + "," + person.age + "," + person.eyeColor;
-console.log(person);
-
-//ini adalah object dan properti ke 2
-const person2 = {
-    name: "Edy Prabowo",
-    age: 17,
-    tall: 180,
-    eyeColor: "Brown"
-};
-
-//pengulangan
-let text = "";
-for (let x in person2) {
-    text += person2[x] + " " + "<br>";
-};
-document.getElementById("p1").innerHTML = text;
-console.log(text);
-
-//ini adalah object dan properti ke 3
-const person3 = {
-    name: "Kazumi",
-    age: 17,
-    tall: 160,
-    eyeColor: "Pink"
-};
-
-//ini menggunakan Object.values()
-const myArray = Object.values(person3);
-
-document.getElementById("p2").innerHTML = myArray;
-console.log(myArray);
-
-//fungsi konstruktor untuk Object Person4
-function person4 (name,age,tall,eye) {
+// fungsi untuk membuat object
+function person (name,age,height,weight) {
     this.name = name;
     this.age = age;
-    this.tall = tall;
-    this.eyeColor = eye;
-    this.nationality = "Indosesia";
+    this.height = height;
+    this.weight = weight;
 }
 
-//membuat 2 Person4 Object
-const ayah = new person4("Kugaa", 51, 185, "Blue");
-const ibu = new person4("Nami", 39, 178, "Brown");
+// membuat array yang berisikan object
+const anak = [
+    new person ("Edy", 17, 170, 60),
+    new person ("Rayhan", 13, 169, 58)
+];
 
-document.getElementById("p3").innerHTML = "Ayahku dari" + ayah.nationality + ". Ibuku dari" + ibu.nationality;
-console.log(person4);
+//pemanggilan di web console
+console.log(anak);
+console.log("Nama:",anak[0].name);
+console.log("Umur:",anak[0].age);
+console.log("TB:",anak[0].height);
+console.log("BB:",anak[0].weight);
+console.log("Nama:",anak[1].name);
+console.log("Umur:",anak[1].age);
+console.log("TB:",anak[1].height);
+console.log("BB:",anak[1].weight);
